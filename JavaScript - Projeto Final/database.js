90f2af5ed5012b766b6ed0b353531a44c45d0e2f
André Alves <73435241+andrealves8@users.noreply.github.com>
2021-07-11 10:43:32 -0300

    add projeto final

---
+++ JavaScript - Projeto Final/database.js
@@ -0,0 +1,40 @@
+const livros = [
+  {
+    id: 1,
+    nome: "Digital Minimalism",
+    autor: "Eduardo Galeano",
+    categoria: "Américas",
+    paginas: 400,
+    recomenda: false,
+    leu: false
+  },
+  {
+    id: 2,
+    nome: "Um estranho em casa",
+    autor: "Leone Parta",
+    categoria: "Drama",
+    paginas: 440,
+    recomenda: false,
+    leu: true
+  },
+  {
+    id: 3,
+    nome: "Amor por acaso",
+    autor: "Jonh Weitel",
+    categoria: "Romance",
+    paginas: 330,
+    recomenda: true,
+    leu: true
+  },
+  {
+    id: 4,
+    nome: "Pânico Total",
+    autor: "Paul",
+    categoria: "Terror",
+    paginas: 450,
+    recomenda: false,
+    leu: true
+  },
+]
+
+module.exports = livros
